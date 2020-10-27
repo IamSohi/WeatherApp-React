e5f4f33913db0e2c8eff094263b0034d51a74135
IamSohi <sukhveersinghsohi70@gmail.com>
2020-10-27 04:46:49 -0700

    Updates

---
+++ precache-manifest.37244c0ef64856d0e272308f3b8aa7d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7534487f819270dc83112abdc3a510c",
+    "url": "/WeatherApp-React/index.html"
+  },
+  {
+    "revision": "3e8d94964876e62e973b",
+    "url": "/WeatherApp-React/static/css/main.bf66c62f.chunk.css"
+  },
+  {
+    "revision": "7d68fad95bba371b25d6",
+    "url": "/WeatherApp-React/static/js/2.e51a8c59.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/WeatherApp-React/static/js/2.e51a8c59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8d94964876e62e973b",
+    "url": "/WeatherApp-React/static/js/main.6a24b2d8.chunk.js"
+  },
+  {
+    "revision": "b9b0e69d22c92bb5a1ad",
+    "url": "/WeatherApp-React/static/js/runtime-main.e1a7d259.js"
+  }
+]);
